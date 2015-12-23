6dba4d61f746cbe91d414f071772c6b5ff18e77f
Are Edvardsen <are.edvardsen@helse-nord.no>
2015-12-23 10:43:22 +0100

    initial commit of load data function for stents

---
+++ R/NORICloadRegDataStent.R
@@ -0,0 +1,30 @@
+#' Provide dataframe for stents
+#'
+#' Provides a dataframe for stents from NORIC staging db. To be used in
+#' NORIC_local_monthly_stent.Rnw and maybe others
+#'
+#' @return RegData data frame
+#' @export
+
+NORICloadRegDataStent <- function() {
+  
+  registryName <- NORICmakeRegistryName()
+  dbType <- "mysql"
+  
+  query <- "
+SELECT
+   A.ForlopsID ,
+   A.ProsedyreType ,
+   A.ProsedyreDato ,
+   SUM(S.ForlopsID>0) AS Nstents
+FROM
+   AngioPCIVar A
+   LEFT JOIN SegmentStent S on A.ForlopsID=S.ForlopsID
+WHERE A.ProsedyreType  != 'Angio'
+GROUP BY ForlopsID;
+"
+  
+  RegData <- rapbase::LoadRegData(registryName, query, dbType)
+  
+  return(RegData)
+}
