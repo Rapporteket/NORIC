519a98067651fb5772a2a8ad5b439d36d9e3d8d0
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-01-08 09:21:16 +0100

    add query for TimeToTreatment-report, hopefully all fields...

--- R/sampleDataGenerator.R
+++ R/sampleDataGenerator.R
@@ -43,7 +43,7 @@ makeSegmentStentSampleData <- function(baseName, reshID) {
 #' @param baseName String giving the prefix base of the name
 #' @param reshID String providing the current reshID. At Rapporteket, reshID
 #'  should already be present in the current R session
-#' @return SegmentStent a data frame holding the data set
+#' @return AngioPCI a data frame holding the data set
 
 makeAngioPCISampleData <- function(baseName, reshID) {
   
@@ -68,4 +68,45 @@ GROUP BY ForlopsID;
   
   return(AngioPCI)
 
+}
+
+
+#' Sample data generator for AngioPCIVar 
+#' 
+#' Functions to generate sample data for development purposes. These data
+#' should be distributed openly, hence it must not hold any restricted
+#' or sensitive information.
+#' 
+#' Resulting data can be saved by:
+#' save([data_frame_name], file="[filename].rda")
+#' 
+#' @param baseName String giving the prefix base of the name
+#' @param reshID String providing the current reshID. At Rapporteket, reshID
+#'  should already be present in the current R session
+#' @return AngioPCIVar a data frame holding the data set
+
+makeTimeToTreatmentSampleData <- function(baseName, reshID) {
+  
+  registryName <- NORICmakeRegistryName(baseName, reshID)
+  query <- "
+SELECT
+  Sykehusnavn ,
+  OverflyttetFraSykehus ,
+  OverflyttetFra ,
+  Indikasjon ,
+  AnkomstPCIDato ,
+  InnleggelseHenvisendeSykehusDato ,
+  ProsedyreDato ,
+  PrimaerForlopsID
+FROM
+   AngioPCIVar;
+  "
+  dbType<-"mysql"
+  AngioPCIVar <- rapbase::LoadRegData(registryName, query, dbType)
+  
+  # remove/change data
+  # no need for it here?
+  
+  return(AngioPCIVar)
+  
 }
