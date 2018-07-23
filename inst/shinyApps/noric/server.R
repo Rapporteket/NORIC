7502d84bae7b9e0b2570eb53d76d4cc82f4c309e
Are Edvardsen <are.edvardsen@helse-nord.no>
2018-07-23 15:29:37 +0200

    shiny app skel

---
+++ inst/shinyApps/noric/server.R
@@ -0,0 +1,55 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  output$distPlot1 <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+  output$markdown <- renderUI({
+    shiny::HTML(markdown::markdownToHTML(knitr::knit("testNoric.Rmd")))
+  })
+  
+  output$downloadReport <- downloadHandler(
+    filename = function() {
+      paste('my-report', sep = '.', switch(
+        input$format, PDF = 'pdf', HTML5 = 'html', Word = 'docx'
+      ))
+    },
+    
+    content = function(file) {
+      src <- normalizePath('testNoric.Rmd')
+      
+      # temporarily switch to the temp dir, in case you do not have write
+      # permission to the current working directory
+      owd <- setwd(tempdir())
+      on.exit(setwd(owd))
+      file.copy(src, 'testNoric.Rmd', overwrite = TRUE)
+      
+      library(rmarkdown)
+      out <- render('testNoric.Rmd', switch(
+        input$format,
+        #PDF = pdf_document(), HTML = html_document(), Word = word_document()
+        PDF = pdf_document(), HTML5 = revealjs::revealjs_presentation(), Word = word_document()
+      ))
+      file.rename(out, file)
+    }
+  )
+})
---
+++ inst/shinyApps/noric/ui.R
@@ -0,0 +1,55 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(
+  navbarPage(
+    title="NORIC",
+    
+    # Application title
+    #titlePanel("NORIC"),
+    
+    tabPanel(
+      "tab1",
+      
+      # Sidebar with a slider input for number of bins
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput(
+            "bins",
+            "Number of bins:",
+            min = 1,
+            max = 50,
+            value = 30
+          )
+        ),
+        mainPanel(
+          plotOutput(
+            "distPlot1"
+          )
+        )
+      )
+    ),
+    tabPanel(
+      "tab2",
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons('format', 'Document format', c('PDF', 'HTML5', 'Word'),
+                       inline = TRUE),
+          downloadButton('downloadReport')
+        ),
+        mainPanel(
+          uiOutput("markdown")
+        )
+      )
+    )
+  )
+)
