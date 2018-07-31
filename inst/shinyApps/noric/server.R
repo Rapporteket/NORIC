7520908a9a1a6d2e589e70d6f3dd09b6e0545268
Are Edvardsen <are.edvardsen@helse-nord.no>
2018-07-31 15:23:35 +0200

    no seagulls

--- inst/shinyApps/noric/server.R
+++ inst/shinyApps/noric/server.R
@@ -72,7 +72,7 @@ shinyServer(function(input, output, session) {
         #PDF = pdf_document(), HTML = html_document(), Word = word_document()
         PDF = pdf_document(),
         HTML = html_document(),
-        BEAMER = beamer_presentation(theme = "seagull"),
+        BEAMER = beamer_presentation(theme = "Hannover"),
         REVEAL = revealjs::revealjs_presentation(theme = "sky")
       ), params = list(tableFormat=switch(
         input$format,
