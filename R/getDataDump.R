9384d6544f3881423544c29dc9511835841bef27
Ellisif Nygaard <ellisif.nygaard@helse-bergen.no>
2020-01-03 15:41:57 +0100

    tilpasset getDataDump slik at 1) man filtrerer basert på den datoen som er mest fornuftig for hver av tabellene (fortsatt SQL-spørring), og 2) de aktuelle feltene fra ForlopsOversikt som vi ønsker i enkelte tabeller legges til vha en left_join.
    Flere tabeller må legges til etter hvert (AnnenDiagnostikk bl.a.).
    
    N.B.! Er usikker på om plassering av raplog::repLogger() er hensiktsmessig

--- R/getDataDump.R
+++ R/getDataDump.R
@@ -6,36 +6,248 @@
 #' @param toDate String with end period, endpont included
 #' @param ... Additional parmeters to be passed to the function
 #'
+#' @importFrom magrittr %>% %<>%
+#' @importFrom dplyr select left_join
+#'
 #' @return A data frame with registry data
 #' @export
 #'
 
 getDataDump <- function(registryName, tableName, fromDate, toDate, ...) {
   
-  query <- paste0("
-SELECT
-  p.*,
-  so.HovedDato
-FROM
-  ", tableName, " p
-LEFT JOIN
-  SkjemaOversikt so
-ON
-  p.ForlopsID = so.ForlopsID AND p.AvdRESH = so.AvdRESH
-WHERE
-  so.HovedDato >= '", fromDate, "' AND so.HovedDato <= '", toDate, "';"
-                  )
-  
-  # special case when dumping these two tables
-  if (tableName %in% c("SkjemaOversikt", "ForlopsOversikt")) {
-    query <- paste0("SELECT * FROM ", tableName, " WHERE HovedDato >= '",
-                    fromDate, "' AND HovedDato <= '", toDate, "';")
+  # Datadumper som skal filtreres på bakgrunn av ProsedyreDato:
+  # AnP, AK, AP, & SS
+  if( tableName %in% c( "AndreProsedyrerVar"
+                        , "AortaklaffVar"
+                        , "AngioPCIVar"
+                        , "SegmentStent") 
+  ){
+    query <- paste0("
+    SELECT
+    p.*
+    FROM 
+    ", tableName, " p 
+    WHERE 
+    p.ProsedyreDato >= '", fromDate, "' AND p.ProsedyreDato <= '", toDate, "';"
+    )
   }
+  
+  
+  # Datadumper som skal filtreres på bakgrunn av HovedDato:
+  # SO & FO
+  if (tableName %in% c( "SkjemaOversikt"
+                        , "ForlopsOversikt")
+  ){
+    query <- paste0("
+    SELECT * 
+    FROM ", tableName, " 
+    WHERE HovedDato >= '", fromDate, "' AND HovedDato <= '", toDate, "';")
+  }
+  
+  
+  # Datadumper som skal filtreres på bakgrunn av UndersokDato:
+  # CT
+  if( tableName %in% c( "CTAngioVar" ) 
+  ){
+    query <- paste0("
+    SELECT
+    p.*
+    FROM 
+    ", tableName, " p 
+    WHERE 
+    p.UndersokDato >= '", fromDate, "' AND p.UndersokDato <= '", toDate, "';"
+    )
+  }
+  
+  
+
   
   if ("session" %in% names(list(...))) {
     raplog::repLogger(session = list(...)[["session"]],
                       msg = paste("NORIC data dump:\n", query))
   }
   
-  rapbase::LoadRegData(registryName, query)
+  
+  # Henter tabellen som skal lastes ned av bruker:
+  tab <- rapbase::LoadRegData(registryName, query)
+  
+  
+  # Henter FO, som har felt som skal legges til tabellen (med unntak av når tabellene som
+  # skal lastes ned er FO, SO, eller PasientStudier)
+    if( tableName %in% c( "AndreProsedyrerVar"
+                          , "AortaklaffVar"
+                          , "AngioPCIVar"
+                          , "CTAngioVar"
+                          , "SegmentStent" )
+  ){
+    
+      
+    FO <- rapbase::LoadRegData(registryName, query = "SELECT * FROM ForlopsOversikt")
+    
+    
+    # De forskjellige tabellene har ulike felt fra FO som de har behov for, og ulike
+    # nøkler. Derfor får hver tabell sin egen if-setning:
+    
+    
+    # AnP
+    if( tableName %in% c( "AndreProsedyrerVar" )){
+      
+      FO %<>% 
+        select(
+          # Nøkler:
+          AvdRESH
+          ,ForlopsID
+          # Variablene som legges til:
+          ,Sykehusnavn
+          ,PasientID
+          ,FodselsDato
+          ,Kommune
+          ,KommuneNr
+          ,Fylke
+          ,Fylkenr
+          ,PasientKjonn
+          ,PasientAlder
+          ,ForlopsType1
+          ,ForlopsType2
+          ,KobletForlopsID
+          ,HovedDato
+        )
+      
+      tab <- left_join(tab, FO, by = c("ForlopsID", "AvdRESH")
+                       , suffix = c("", ".FO") # usikker som denne linjen trengs, slette senere?
+                       )
+    }
+    
+    # AK
+    if( tableName %in% c( "AortaklaffVar" )){
+      
+      FO %<>% 
+        select(
+          # Nøkler:
+          AvdRESH
+          ,ForlopsID
+          # Variablene som legges til:
+          ,Sykehusnavn
+          ,PasientID
+          ,FodselsDato
+          ,Kommune
+          ,KommuneNr
+          ,Fylke
+          ,Fylkenr
+          ,PasientKjonn
+          ,PasientAlder
+          ,ForlopsType1
+          ,ForlopsType2
+          ,KobletForlopsID
+          ,HovedDato
+        )
+      
+      tab <- left_join(tab, FO, by = c("ForlopsID", "AvdRESH")
+                       , suffix = c("", ".FO") # usikker som denne linjen trengs, slette senere?
+      )
+    }
+    
+    
+    # AP
+    if( tableName %in% c( "AngioPCIVar" ) ){
+      
+      FO %<>% 
+        select(
+          # Nøkler:
+          AvdRESH
+          ,Sykehusnavn
+          ,PasientID
+          ,ForlopsID
+          # FodselsDato # Finnes per dags dato i AP fra før
+          # Variablene som legges til:
+          ,Kommune
+          ,KommuneNr
+          ,Fylke
+          ,Fylkenr
+          # ,PasientKjonn # Finnes per dags dato i AP (heter ikke PasientKjonn, men Kjonn)
+          ,PasientAlder
+          ,ForlopsType1
+          ,ForlopsType2
+          ,KobletForlopsID
+          ,HovedDato
+        )
+      
+      # Legger til variabler fra FO til AP:
+      tab <- left_join(tab, FO, by = c("AvdRESH"
+                                       ,"Sykehusnavn"
+                                       ,"PasientID"
+                                       ,"ForlopsID") 
+      ) 
+    }
+    
+    
+    # CT
+    if( tableName %in% c( "CTAngioVar" ) ){
+      
+      FO %<>% 
+        select(
+          # Nøkler:
+          AvdRESH
+          ,ForlopsID
+          ,PasientID
+          # Variablene som legges til:
+          ,Sykehusnavn
+          # ,FodselsDato # Finnes per d.d. i CT
+          ,Kommune
+          ,KommuneNr
+          ,Fylke
+          ,Fylkenr
+          ,PasientKjonn
+          ,PasientAlder
+          ,ForlopsType1
+          ,ForlopsType2
+          ,KobletForlopsID
+          ,HovedDato
+        )
+      
+      tab <- left_join(tab, FO, by = c("ForlopsID"
+                                       , "AvdRESH"
+                                       , "PasientID"),
+                      suffix = c("", ".FO") # usikker som denne linjen trengs, slette senere?
+                      )
+    }
+    
+    
+    
+    # SS
+    if( tableName %in% c( "SegmentStent" ) ){
+      
+      FO %<>% 
+        select(
+          # Nøkler:
+          AvdRESH
+          ,ForlopsID
+          ,Sykehusnavn
+          # Variablene som legges til:
+          ,PasientID
+          # ,FodselsDato # Finnes per d.d. i SS
+          ,Kommune
+          ,KommuneNr
+          ,Fylke
+          ,Fylkenr
+          # ,PasientKjonn # Finnes per d.d. i SS
+          ,PasientAlder
+          ,ForlopsType1
+          ,ForlopsType2
+          ,KobletForlopsID
+          ,HovedDato
+        )
+      
+      tab <- left_join(tab, FO, by = c("ForlopsID"
+                                       , "AvdRESH"
+                                       , "Sykehusnavn"),
+                      suffix = c("", ".FO") # usikker som denne linjen trengs, slette senere?
+                      )
+    }
+  }
+  
+  
+  # Returnerer tabell (med eller uten felt fra FO)
+  return( tab )
+  
 }
