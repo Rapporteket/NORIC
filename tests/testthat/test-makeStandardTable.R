4438258fe29a2e0abc4d16a151af91f11db618e1
Are Edvardsen <biorakel@gmail.com>
2020-11-24 14:38:35 +0100

    free coverage...

--- tests/testthat/test-makeStandardTable.R
+++ tests/testthat/test-makeStandardTable.R
@@ -1,6 +1,8 @@
 test_that("function returns kable objects", {
   expect_true("kableExtra" %in% class(mst(tab = mtcars[1:10, ],
                                           type = "html")))
-  expect_true("knitr_kable" %in% class(mst(tab = mtcars[1:10, ])))
+  expect_true("knitr_kable" %in% class(mst(tab = mtcars[1:10, ], lsd = TRUE)))
   expect_true("matrix" %in% class(prettyTab(as.matrix(mtcars[1:10, ]))))
+  expect_true("matrix" %in% class(prettyTab(as.matrix(mtcars[1:10, ]),
+                                            add_totals = TRUE)))
 })
