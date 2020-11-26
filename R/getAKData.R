37ac97e61611930546058201896ad3d81a9da87d
Are Edvardsen <biorakel@gmail.com>
2020-11-26 11:33:06 +0100

    fix lint complaints

--- R/getAKData.R
+++ R/getAKData.R
@@ -21,7 +21,7 @@ getAKData <- function(registryName, singleRow = FALSE, ...) {
   . <- ""
 
   dbType <- "mysql"
-  query <-"
+  query <- "
 SELECT
   *
 FROM
@@ -30,104 +30,105 @@ FROM
 
   if (singleRow) {
     query <- paste0(query, "\nLIMIT\n  1;")
-    msg = "Query metadata for AortaklaffVar pivot"
+    msg <- "Query metadata for AortaklaffVar pivot"
   } else {
     query <- paste0(query, ";")
-    msg = "Query data for AortaklaffVar pivot"
+    msg <- "Query data for AortaklaffVar pivot"
   }
 
   if ("session" %in% names(list(...))) {
     raplog::repLogger(session = list(...)[["session"]], msg = msg)
   }
 
-  AK <- rapbase::LoadRegData(registryName, query, dbType)
+  aK <- rapbase::LoadRegData(registryName, query, dbType)
 
-  FO <- rapbase::LoadRegData(registryName,
+  fO <- rapbase::LoadRegData(registryName,
                              query = "SELECT * FROM ForlopsOversikt")
 
 
-  # Velger relevante variabler fra FO som skal legges til tabellen:
-  FO %<>%
+  # Velger relevante variabler fra fO som skal legges til tabellen:
+  fO %<>%
     dplyr::select(.,
       # Nøkler:
-      .data$AvdRESH
-      ,.data$ForlopsID
+      .data$AvdRESH,
+      .data$ForlopsID,
       # Variablene som legges til:
-      ,.data$Sykehusnavn
-      ,.data$PasientID
-      ,.data$FodselsDato
-      ,.data$Kommune
-      ,.data$KommuneNr
-      ,.data$Fylke
-      ,.data$Fylkenr
-      ,.data$PasientKjonn
-      ,.data$PasientAlder
-      ,.data$ForlopsType1
-      ,.data$ForlopsType2
-      ,.data$KobletForlopsID
-      ,.data$HovedDato
+      .data$Sykehusnavn,
+      .data$PasientID,
+      .data$FodselsDato,
+      .data$Kommune,
+      .data$KommuneNr,
+      .data$Fylke,
+      .data$Fylkenr,
+      .data$PasientKjonn,
+      .data$PasientAlder,
+      .data$ForlopsType1,
+      .data$ForlopsType2,
+      .data$KobletForlopsID,
+      .data$HovedDato
     )
 
 
-  AK <- dplyr::left_join(AK, FO, by = c("ForlopsID", "AvdRESH"),
-                         suffix = c("", ".FO"))
+  aK <- dplyr::left_join(aK, fO, by = c("ForlopsID", "AvdRESH"),
+                         suffix = c("", ".fO"))
 
 
 
   # Gjor datoer om til dato-objekt:
-  AK %<>%
+  aK %<>%
     dplyr::mutate_at(
-      vars( ends_with("dato", ignore.case = TRUE) ), list( ymd )
+      vars(ends_with("dato", ignore.case = TRUE)), list(ymd)
     )
 
 
   # Utledete variabler:
-  AK %<>%
+  aK %<>%
     dplyr::mutate(.,
 
       dager_mellom_prosedyre_og_utskr = as.numeric(
-        difftime(  .data$UtskrDato, .data$ProsedyreDato, units = "days" )
-        )
+        difftime(.data$UtskrDato, .data$ProsedyreDato, units = "days")),
 
       # Div. tidsvariabler:
       #
       # Kalenderår for ProsedyreDato:
-      ,aar = as.ordered( year( .data$ProsedyreDato ))
+      aar = as.ordered(year(.data$ProsedyreDato)),
       # Måned:
       # (månedsnr er tosifret; 01, 02, ....)
-      ,maaned_nr = as.ordered( sprintf(fmt = "%02d", month( .data$ProsedyreDato ) ))
-      ,maaned = as.ordered( paste0( .data$aar, "-", .data$maaned_nr) )
+      maaned_nr = as.ordered(sprintf(fmt = "%02d",
+                                     month(.data$ProsedyreDato))),
+      maaned = as.ordered(paste0(.data$aar, "-", .data$maaned_nr)),
       # Kvartal:
-      ,kvartal = quarter( .data$ProsedyreDato, with_year = TRUE )
-      # kvartal = as.factor( gsub( "\\.", "-", kvartal) )
-      ,kvartal = as.ordered( gsub( "[[:punct:]]", "-Q", .data$kvartal) )
+      kvartal = quarter(.data$ProsedyreDato, with_year = TRUE),
+      kvartal = as.ordered(gsub("[[:punct:]]", "-Q", .data$kvartal)),
       # Uketall:
-      ,uke = as.ordered( sprintf(fmt = "%02d", isoweek( .data$ProsedyreDato ) ))
+      uke = as.ordered(sprintf(fmt = "%02d", isoweek(.data$ProsedyreDato))),
 
       # Variabel med "yyyy-ukenummer" som tar høyde for uketall spredt over to
       # kalenderår:
 
-      ,aar_uke = ifelse(
+      aar_uke = ifelse(
         # hvis uke 01 er i desember...
-        test = .data$uke == "01" & .data$maaned_nr == "12"
+        test = .data$uke == "01" & .data$maaned_nr == "12",
         # .. så sier vi at uken tilhører det seneste av de to årene som uke 01
         # er spredt over (uke 01 i desember 2019 blir til 2020-01)
-        , yes = paste0( as.integer(year(.data$ProsedyreDato)) + 1, "-", .data$uke )
-        , no = paste0(.data$aar, "-", .data$uke )
-      )
-      ,aar_uke = ifelse(
+        yes = paste0(as.integer(year(.data$ProsedyreDato)) + 1, "-",
+                     .data$uke),
+        no = paste0(.data$aar, "-", .data$uke)
+      ),
+      aar_uke = ifelse(
         # hvis uke 52 eller 53 er i januar...
-        test = .data$uke %in% c("52", "53") & .data$maaned_nr == "01"
+        test = .data$uke %in% c("52", "53") & .data$maaned_nr == "01",
         # ...sier vi at hele uken tilhører det tidligste av de to årene som uke
-        # 52/53 er spredt over (1. januar 2017 som er i uke 52 blir til 2016-52)
-        , yes = paste0( as.integer(year(.data$ProsedyreDato)) - 1, "-", .data$uke )
-        , no = .data$aar_uke
-      )
-      ,aar_uke = as.ordered( .data$aar_uke )
+        # 52/53 er spredt over (1.januar 2017 som er i uke 52 blir til 2016-52)
+        yes = paste0(as.integer(year(.data$ProsedyreDato)) - 1, "-",
+                     .data$uke),
+        no = .data$aar_uke
+      ),
+      aar_uke = as.ordered(.data$aar_uke)
     )
 
 
 
-  AK
+  aK
 
 }
