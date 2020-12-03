d005bec918cad907068b7cdffd4a673beb5a02b9
Are Edvardsen <biorakel@gmail.com>
2020-12-03 09:43:53 +0100

    better code

--- R/getLocalSSData.R
+++ R/getLocalSSData.R
@@ -5,23 +5,17 @@
 #' relevant usecase will be when only description is needed. By default set to
 #' FALSE
 #' @param ... Optional arguments to be passed to the function
-#'
-#' @importFrom magrittr %>% %<>%
 #' @importFrom dplyr arrange count filter group_by left_join mutate mutate_at
 #' select ungroup
 #' @importFrom lubridate isoweek month quarter year ymd
-#'
 #' @return Data frame representing the table SegmentStent
 #' @export
 #'
 
 getLocalSSData <- function(registryName, singleRow = FALSE, ...) {
 
-  # declare 'dot'
-  . <- ""
-
   dbType <- "mysql"
-  query <-"
+  query <- "
 SELECT
   *
 FROM
@@ -30,183 +24,206 @@ FROM
 
   if (singleRow) {
     query <- paste0(query, "\nLIMIT\n  1;")
-    msg = "Query metadata for SegmentStent pivot"
+    msg <- "Query metadata for SegmentStent pivot"
   } else {
     query <- paste0(query, ";")
-    msg = "Query data for SegmentStent pivot"
+    msg <- "Query data for SegmentStent pivot"
   }
 
   if ("session" %in% names(list(...))) {
     raplog::repLogger(session = list(...)[["session"]], msg = msg)
   }
 
-  SS <- rapbase::loadRegData(registryName, query, dbType)
+  sS <- rapbase::loadRegData(registryName, query, dbType)
 
-  FO <- rapbase::loadRegData(registryName,
+  fO <- rapbase::loadRegData(registryName,
                              query = "SELECT * FROM ForlopsOversikt")
 
-  # Velger relevante variabler fra FO som skal legges til tabellen:
-  FO %<>%
-    select(
+  # Velger relevante variabler fra fO som skal legges til tabellen:
+  fO %<>%
+    dplyr::select(
       # Nøkler:
-      AvdRESH
-      ,ForlopsID
-      ,Sykehusnavn
+      .data$AvdRESH,
+      .data$ForlopsID,
+      .data$Sykehusnavn,
       # Variablene som legges til:
-      ,PasientID
-      # ,FodselsDato # Finnes per d.d. i SS
-      ,Kommune
-      ,KommuneNr
-      ,Fylke
-      ,Fylkenr
-      # ,PasientKjonn # Finnes per d.d. i SS
-      ,PasientAlder
-      ,ForlopsType1
-      ,ForlopsType2
-      ,KobletForlopsID
+      .data$PasientID,
+      .data$Kommune,
+      .data$KommuneNr,
+      .data$Fylke,
+      .data$Fylkenr,
+      .data$PasientAlder,
+      .data$ForlopsType1,
+      .data$ForlopsType2,
+      .data$KobletForlopsID
     )
 
-  SS <- left_join(SS, FO, by = c("ForlopsID", "AvdRESH", "Sykehusnavn"),
-                  suffix = c("", ".FO"))
+  sS <- dplyr::left_join(sS, fO, by = c("ForlopsID", "AvdRESH", "Sykehusnavn"),
+                         suffix = c("", ".fO"))
 
   # Gjor datoer om til dato-objekt:
-  SS %<>%
-    mutate_at(
-      vars( ends_with("dato", ignore.case = TRUE) ), list( ymd )
+  sS %<>%
+    dplyr::mutate_at(
+      vars(ends_with("dato", ignore.case = TRUE)), list(lubridate::ymd)
     )
 
 
   # Endre Sykehusnavn til kortere versjoner:
-  SS %<>%
-    mutate(
-      Sykehusnavn = ifelse( Sykehusnavn == "Haukeland" , "HUS" , Sykehusnavn ) ,
+  sS %<>%
+    dplyr::mutate(
       Sykehusnavn = ifelse(
-        Sykehusnavn %in% c("St.Olav", "St. Olav") , "St.Olavs"  , Sykehusnavn
-      ) ,
+        .data$Sykehusnavn == "Haukeland",
+        "HUS",
+        .data$Sykehusnavn),
       Sykehusnavn = ifelse(
-        Sykehusnavn == "Akershus universitetssykehus HF" , "Ahus" , Sykehusnavn
-      )
+        .data$Sykehusnavn %in% c("St.Olav", "St. Olav"),
+        "St.Olavs",
+        .data$Sykehusnavn),
+      Sykehusnavn = ifelse(
+        .data$Sykehusnavn == "Akershus universitetssykehus HF",
+        "Ahus",
+        .data$Sykehusnavn)
     )
 
   # Tar bort forløp fra før sykehusene ble offisielt med i NORIC (potensielle
   # "tøyseregistreringer")
-  SS %<>%
-    filter(
+  sS %<>%
+    dplyr::filter(
       (
-        (AvdRESH == 102966) & ( as.Date(ProsedyreDato) >= "2013-01-01" ) # HUS
+        # HUS
+        (.data$AvdRESH == 102966) & (as.Date(.data$ProsedyreDato) >=
+                                       "2013-01-01")
       ) | (
-        (AvdRESH == 101619) & ( as.Date(ProsedyreDato) >= "2013-05-01" ) # UNN
+        # UNN
+        (.data$AvdRESH == 101619) & (as.Date(.data$ProsedyreDato) >=
+                                       "2013-05-01")
       ) | (
-        (AvdRESH == 109880) & ( as.Date(ProsedyreDato) >= "2014-01-01" ) # Ullevål
+        # Ullevaal
+        (.data$AvdRESH == 109880) & (as.Date(.data$ProsedyreDato) >=
+                                       "2014-01-01")
       ) | (
-        (AvdRESH == 104284) & ( as.Date(ProsedyreDato) >= "2014-01-01" ) # St.Olavs
+        # St Olav
+        (.data$AvdRESH == 104284) & (as.Date(.data$ProsedyreDato) >=
+                                       "2014-01-01")
       ) | (
-        (AvdRESH == 114150) & ( as.Date(ProsedyreDato) >= "2014-01-01" ) # SSA
+        #SSA
+        (.data$AvdRESH == 114150) & (as.Date(.data$ProsedyreDato) >=
+                                       "2014-01-01")
       ) | (
-        (AvdRESH == 105502) & ( as.Date(ProsedyreDato) >= "2014-01-01" ) # SUS
+        #SUS
+        (.data$AvdRESH == 105502) & (as.Date(.data$ProsedyreDato) >=
+                                       "2014-01-01")
       ) | (
-        (AvdRESH == 700422) & ( as.Date(ProsedyreDato) >= "2015-01-01" ) # Riksen
+        # Riksen
+        (.data$AvdRESH == 700422) & (as.Date(.data$ProsedyreDato) >=
+                                       "2015-01-01")
       ) | (
-        (AvdRESH == 106944) & ( as.Date(ProsedyreDato) >= "2015-01-01" ) # LHLGardermoen
+        # LHL Gardermoen
+        (.data$AvdRESH == 106944) & (as.Date(.data$ProsedyreDato) >=
+                                       "2015-01-01")
       ) | (
-        (AvdRESH == 108141) & ( as.Date(ProsedyreDato) >= "2016-01-01" ) # Ahus
+        # Ahus
+        (.data$AvdRESH == 108141) & (as.Date(.data$ProsedyreDato) >=
+                                       "2016-01-01")
       ) | (
-        (AvdRESH == 4210141) & ( as.Date(ProsedyreDato) >= "2020-02-10" ) # Bodø
+        # Bodoe
+        (.data$AvdRESH == 4210141) & (as.Date(.data$ProsedyreDato) >=
+                                        "2020-02-10")
       )
     )
 
 
   # Gjøre kategoriske variabler om til factor:
   # (ikke fullstendig, må legge til mer etter hvert)
-  SS %<>%
-    mutate(
-      Graft = factor( Graft,
-                      levels = c(
-                        "Nei"
-                        ,"Arteriell"
-                        ,"Vene"
-                      )
-                      ,exclude = NULL # inkluderer NA i levels
-                      ,ordered = TRUE
+  sS %<>%
+    dplyr::mutate(
+      Graft = factor(.data$Graft,
+                     levels = c(
+                       "Nei",
+                       "Arteriell",
+                       "Vene"
+                     ),
+                     exclude = NULL, # inkluderer NA i levels
+                     ordered = TRUE
+      ),
+
+      Stenoseklasse = factor(.data$Stenoseklasse,
+                             levels = c(
+                               "A",
+                               "B1",
+                               "B1 Bifurkasjon",
+                               "B2",
+                               "B2 Bifurkasjon",
+                               "C",
+                               "C Bifurkasjon",
+                               "Annet"
+                             ),
+                             ordered = TRUE
+      ),
+
+      StenoseType = factor(.data$StenoseType,
+                           levels = c(
+                             "DeNovo",
+                             "In-stent restenose",
+                             "Stenttrombose",
+                             "Andre restenoser"
+                           ),
+                           ordered = TRUE
       )
-
-      ,Stenoseklasse = factor( Stenoseklasse,
-                              levels = c(
-                                "A"
-                                ,"B1"
-                                ,"B1 Bifurkasjon"
-                                ,"B2"
-                                ,"B2 Bifurkasjon"
-                                ,"C"
-                                ,"C Bifurkasjon"
-                                ,"Annet"
-                              ),
-                              ordered = TRUE
-                              )
-
-      ,StenoseType = factor( StenoseType,
-                              levels = c(
-                                "DeNovo"
-                                ,"In-stent restenose"
-                                ,"Stenttrombose "
-                                ,"Andre restenoser"
-                              ),
-                              ordered = TRUE
-                              )
     )
 
 
   # Utledete variabler:
-  SS %<>%
-    mutate(
+  sS %<>%
+    dplyr::mutate(
       # Div. tidsvariabler:
       #
       # Kalenderår for ProsedyreDato:
-      aar = as.ordered( year( ProsedyreDato ))
+      aar = as.ordered(lubridate::year(.data$ProsedyreDato)),
       # Måned:
       # (månedsnr er tosifret; 01, 02, ....)
-      ,maaned_nr = as.ordered( sprintf(fmt = "%02d", month( ProsedyreDato ) ))
-      ,maaned = as.ordered( paste0( aar, "-", maaned_nr) )
+      maaned_nr = as.ordered(sprintf(fmt = "%02d",
+                                     lubridate::month(.data$ProsedyreDato))),
+      maaned = as.ordered(paste0(.data$aar, "-", .data$maaned_nr)),
       # Kvartal:
-      ,kvartal = quarter( ProsedyreDato, with_year = TRUE )
-      # kvartal = as.factor( gsub( "\\.", "-", kvartal) )
-      ,kvartal = as.ordered( gsub( "[[:punct:]]", "-Q", kvartal) )
+      kvartal = lubridate::quarter(.data$ProsedyreDato, with_year = TRUE),
+      kvartal = as.ordered(gsub("[[:punct:]]", "-Q", .data$kvartal)),
       # Uketall:
-      ,uke = as.ordered( sprintf(fmt = "%02d", isoweek( ProsedyreDato ) ))
-
+      uke = as.ordered(sprintf(fmt = "%02d",
+                               lubridate::isoweek(.data$ProsedyreDato))),
       # Variabel med "yyyy-ukenummer" som tar høyde for uketall spredt over to
       # kalenderår:
-
-      ,aar_uke = ifelse(
+      aar_uke = ifelse(
         # hvis uke 01 er i desember...
-        test = uke == "01" & maaned_nr == "12"
+        test = .data$uke == "01" & .data$maaned_nr == "12",
         # .. så sier vi at uken tilhører det seneste av de to årene som uke 01
         # er spredt over (uke 01 i desember 2019 blir til 2020-01)
-        , yes = paste0( as.integer(year(ProsedyreDato)) + 1, "-", uke )
-        , no = paste0(aar, "-", uke )
-      )
-      ,aar_uke = ifelse(
+        yes = paste0(as.integer(lubridate::year(.data$ProsedyreDato)) + 1, "-",
+                     .data$uke),
+        no = paste0(.data$aar, "-", .data$uke)
+      ),
+      aar_uke = ifelse(
         # hvis uke 52 eller 53 er i januar...
-        test = uke %in% c("52", "53") & maaned_nr == "01"
+        test = .data$uke %in% c("52", "53") & .data$maaned_nr == "01",
         # ...sier vi at hele uken tilhører det tidligste av de to årene som uke
         # 52/53 er spredt over (1. januar 2017 som er i uke 52 blir til 2016-52)
-        , yes = paste0( as.integer(year(ProsedyreDato)) - 1, "-", uke )
-        , no = aar_uke
-      )
-      ,aar_uke = as.ordered( aar_uke )
+        yes = paste0(as.integer(lubridate::year(.data$ProsedyreDato)) - 1, "-",
+                     .data$uke),
+        no = .data$aar_uke
+      ),
+      aar_uke = as.ordered(.data$aar_uke)
     )
 
 
   # Utledet variabel:
   # ant_stent_ila_forlop = antall stenter satt inn ila ett forløp
 
-  SS %<>%
-    group_by( Sykehusnavn, ForlopsID ) %>%
-    mutate( antall_stent_ila_forlop = sum( !is.na(StentType) ) ) %>%
+  sS %<>%
+    dplyr::group_by(.data$Sykehusnavn, .data$ForlopsID) %>%
+    dplyr::mutate(antall_stent_ila_forlop = sum(!is.na(.data$StentType))) %>%
     dplyr::ungroup() %>%
-    arrange( Sykehusnavn, ForlopsID )
+    dplyr::arrange(.data$Sykehusnavn, .data$ForlopsID)
 
-
-  SS
+  sS
 
 }
