b2a8eb8125a22e795e520a8dde02292b3eb92add
Are Edvardsen <biorakel@gmail.com>
2020-12-03 08:27:18 +0100

    better code

--- R/getLocalSOData.R
+++ R/getLocalSOData.R
@@ -16,125 +16,136 @@
 
 getLocalSOData <- function(registryName, singleRow = FALSE, ...) {
 
-  # declare 'dot'
-  . <- ""
-
   dbType <- "mysql"
   query <- "SELECT * FROM SkjemaOversikt"
 
   if (singleRow) {
     query <- paste0(query, "\nLIMIT\n  1;")
-    msg = "Query metadata for SkjemaOversikt pivot"
+    msg <- "Query metadata for SkjemaOversikt pivot"
   } else {
     query <- paste0(query, ";")
-    msg = "Query data for SkjemaOversikt pivot"
+    msg <- "Query data for SkjemaOversikt pivot"
   }
 
   if ("session" %in% names(list(...))) {
     raplog::repLogger(session = list(...)[["session"]], msg = msg)
   }
 
-  SO <- rapbase::loadRegData(registryName, query, dbType)
+  sO <- rapbase::loadRegData(registryName, query, dbType)
 
 
 
   # Gjor datoer om til dato-objekt:
-  SO %<>%
-    mutate(
-      OpprettetDato = ymd_hms( OpprettetDato )
-      ,SistLagretDato = ymd_hms( SistLagretDato )
-      ,HovedDato = ymd( HovedDato )
+  sO %<>%
+    dplyr::mutate(
+      OpprettetDato = lubridate::ymd_hms(.data$OpprettetDato),
+      SistLagretDato = lubridate::ymd_hms(.data$SistLagretDato),
+      HovedDato = lubridate::ymd(.data$HovedDato)
     )
 
 
   # Endre Sykehusnavn til kortere versjoner:
-  SO %<>%
-    mutate(
-      Sykehusnavn = ifelse( Sykehusnavn == "Haukeland" , "HUS" , Sykehusnavn ) ,
+  sO %<>%
+    dplyr::mutate(
       Sykehusnavn = ifelse(
-        Sykehusnavn %in% c("St.Olav", "St. Olav") , "St.Olavs"  , Sykehusnavn
-      ) ,
+        .data$Sykehusnavn == "Haukeland",
+        "HUS",
+        .data$Sykehusnavn),
       Sykehusnavn = ifelse(
-        Sykehusnavn == "Akershus universitetssykehus HF" , "Ahus" , Sykehusnavn
-      )
+        .data$Sykehusnavn %in% c("St.Olav", "St. Olav"),
+        "St.Olavs",
+        .data$Sykehusnavn),
+      Sykehusnavn = ifelse(
+        .data$Sykehusnavn == "Akershus universitetssykehus HF",
+        "Ahus",
+        .data$Sykehusnavn)
     )
 
 
   # Tar bort forløp fra før sykehusene ble offisielt med i NORIC (potensielle
   # "tøyseregistreringer")
-  SO %<>%
-    filter(
+  sO %<>%
+    dplyr::filter(
       (
-        (AvdRESH == 102966) & ( as.Date(HovedDato) >= "2013-01-01" ) # HUS
+        # HUS
+        (.data$AvdRESH == 102966) & (as.Date(.data$HovedDato) >= "2013-01-01")
       ) | (
-        (AvdRESH == 101619) & ( as.Date(HovedDato) >= "2013-05-01" ) # UNN
+        # UNN
+        (.data$AvdRESH == 101619) & (as.Date(.data$HovedDato) >= "2013-05-01")
       ) | (
-        (AvdRESH == 109880) & ( as.Date(HovedDato) >= "2014-01-01" ) # Ullevål
+        # Ullevaal
+        (.data$AvdRESH == 109880) & (as.Date(.data$HovedDato) >= "2014-01-01")
       ) | (
-        (AvdRESH == 104284) & ( as.Date(HovedDato) >= "2014-01-01" ) # St.Olavs
+        # St Olav
+        (.data$AvdRESH == 104284) & (as.Date(.data$HovedDato) >= "2014-01-01")
       ) | (
-        (AvdRESH == 114150) & ( as.Date(HovedDato) >= "2014-01-01" ) # SSA
+        # SSA
+        (.data$AvdRESH == 114150) & (as.Date(.data$HovedDato) >= "2014-01-01")
       ) | (
-        (AvdRESH == 105502) & ( as.Date(HovedDato) >= "2014-01-01" ) # SUS
+        # SUS
+        (.data$AvdRESH == 105502) & (as.Date(.data$HovedDato) >= "2014-01-01")
       ) | (
-        (AvdRESH == 700422) & ( as.Date(HovedDato) >= "2015-01-01" ) # Riksen
+        # Risksen
+        (.data$AvdRESH == 700422) & (as.Date(.data$HovedDato) >= "2015-01-01")
       ) | (
-        (AvdRESH == 106944) & ( as.Date(HovedDato) >= "2015-01-01" ) # LHLGardermoen
+        # LHL Gardermoen
+        (.data$AvdRESH == 106944) & (as.Date(.data$HovedDato) >= "2015-01-01")
       ) | (
-        (AvdRESH == 108141) & ( as.Date(HovedDato) >= "2016-01-01" ) # Ahus
+        # Ahus
+        (.data$AvdRESH == 108141) & (as.Date(.data$HovedDato) >= "2016-01-01")
       ) | (
-        (AvdRESH == 4210141) & ( as.Date(HovedDato) >= "2020-02-10" ) # Bodø
+        # Bodø
+        (.data$AvdRESH == 4210141) & (as.Date(.data$HovedDato) >= "2020-02-10")
       )
     )
-
-
 
 
   # Utledete variabler:
-  SO %<>%
-    mutate(
+  sO %<>%
+    dplyr::mutate(
       # Ferdigstilt, 1= ja, -1 & 0 = nei
-      ferdigstilt = as.ordered( ifelse(SkjemaStatus == 1
-                                       , yes = "Ferdigstilt"
-                                       , no = "Ikke ferdigstilt")
-                                ),
+      ferdigstilt = as.ordered(ifelse(.data$SkjemaStatus == 1,
+                                      yes = "Ferdigstilt",
+                                      no = "Ikke ferdigstilt")
+      ),
       # Div. tidsvariabler:
       #
       # Kalenderår for HovedDato:
-      aar = as.ordered( year( HovedDato ))
+      aar = as.ordered(lubridate::year(.data$HovedDato)),
       # Måned:
       # (månedsnr er tosifret; 01, 02, ....)
-      ,maaned_nr = as.ordered( sprintf(fmt = "%02d", month( HovedDato ) ))
-      ,maaned = as.ordered( paste0( aar, "-", maaned_nr) )
+      maaned_nr = as.ordered(sprintf(fmt = "%02d",
+                                     lubridate::month(.data$HovedDato))),
+      maaned = as.ordered(paste0(.data$aar, "-", .data$maaned_nr)),
       # Kvartal:
-      ,kvartal = quarter( HovedDato, with_year = TRUE )
-      # kvartal = as.factor( gsub( "\\.", "-", kvartal) )
-      ,kvartal = as.ordered( gsub( "[[:punct:]]", "-Q", kvartal) )
+      kvartal = lubridate::quarter(.data$HovedDato, with_year = TRUE),
+      kvartal = as.ordered(gsub("[[:punct:]]", "-Q", .data$kvartal)),
       # Uketall:
-      ,uke = as.ordered( sprintf(fmt = "%02d", isoweek( HovedDato ) ))
-
+      uke = as.ordered(sprintf(fmt = "%02d",
+                               lubridate::isoweek(.data$HovedDato))),
       # Variabel med "yyyy-ukenummer" som tar høyde for uketall spredt over to
       # kalenderår:
-
-      ,aar_uke = ifelse(
+      aar_uke = ifelse(
         # hvis uke 01 er i desember...
-        test = uke == "01" & maaned_nr == "12"
+        test = .data$uke == "01" & .data$maaned_nr == "12",
         # .. så sier vi at uken tilhører det seneste av de to årene som uke 01
         # er spredt over (uke 01 i desember 2019 blir til 2020-01)
-        , yes = paste0( as.integer(year(HovedDato)) + 1, "-", uke )
-        , no = paste0(aar, "-", uke )
-      )
-      ,aar_uke = ifelse(
+        yes = paste0(as.integer(lubridate::year(.data$HovedDato)) + 1, "-",
+                     .data$uke),
+        no = paste0(.data$aar, "-", .data$uke)
+      ),
+      aar_uke = ifelse(
         # hvis uke 52 eller 53 er i januar...
-        test = uke %in% c("52", "53") & maaned_nr == "01"
+        test = .data$uke %in% c("52", "53") & .data$maaned_nr == "01",
         # ...sier vi at hele uken tilhører det tidligste av de to årene som uke
         # 52/53 er spredt over (1. januar 2017 som er i uke 52 blir til 2016-52)
-        , yes = paste0( as.integer(year(HovedDato)) - 1, "-", uke )
-        , no = aar_uke
-      )
-      ,aar_uke = as.ordered( aar_uke )
+        yes = paste0(as.integer(lubridate::year(.data$HovedDato)) - 1, "-",
+                        .data$uke),
+        no = .data$aar_uke
+      ),
+      aar_uke = as.ordered(.data$aar_uke)
     )
 
-  SO
+  sO
 
-  }
+}
