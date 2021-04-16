8adeebf46e0d095238e9655d57f7d3bb8fea96ea
Are Edvardsen <biorakel@gmail.com>
2021-04-16 12:17:28 +0200

    new fun for newest entry

---
+++ R/getLatestEntry.R
@@ -0,0 +1,20 @@
+#' Get the latest registration date from the registry database
+#' 
+#' Provides the newest date based on HovedDato from SkjemaOversikt
+#'
+#' @param registryName 
+#'
+#' @return A Date object
+#' @export
+
+getLatesEntry <- function(registryName) {
+  
+  query <- paste0("
+SELECT
+  max(HovedDato) AS date
+FROM
+  SkjemaOversikt;"
+                  )
+  
+  rapbase::loadRegData(registryName, query = query)$date
+}
