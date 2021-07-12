767a62fd72eeb54d8f9caf7b16de7e7770bd80f0
Kristina Skåre <kristina.skare@helse-bergen.no>
2021-07-12 10:05:20 +0200

    clan-up with examples

--- R/utlede_variabler.R
+++ R/utlede_variabler.R
@@ -1,17 +1,18 @@
 
 
 
-#' Utlede aldersklasse -tiår
-#' PasientAlder under 18 år eller over 99 år settes som NA.
+#' Add variable `aldersklasse` - decades
 #'
-#' @param df data.frame der variablene skal legges til, må inneholde en
-#' numerisk variabel som inneholder pasientens alder.
-#' @param var default verdi er `PasientAlder`
+#' Under 18 years og above 99 are set as NA
+#' @param df data.frame where the new variable should be added.
+#' @param var Variable name of the numerical variable that contains age as
+#' a continuous value. Default variable name is `PasientAlder`
+#' @return data.frame with new variable
 #'
-#' @return data.frame med en ny variabel
 #' @export
-#'
-#' @examples
+#' @example
+#' df <-  data.frame(age = c(10,20,59,60, NA, 61,69,70,99,100))
+#' df %>% utlede_aldersklasse(., var = age)
 utlede_aldersklasse <- function(df, var = PasientAlder){
   df %>%
     dplyr::mutate(
@@ -26,16 +27,25 @@ utlede_aldersklasse <- function(df, var = PasientAlder){
 
 
 
-#' Utlede binære variaber ("ja", "nei") for ferdigstilte skjema
+#' Add binary variable ("ja", "nei") for table status
 #'
-#' @param df data.frame der variablene skal legges til
-#' @param var variablel som inneholder skjemastatus på numerisk form {-1,0,1}
-#' @param suffix ønsket suffix i nytt variabelnavn:  ferdigstilt_`suffix`
+#' @param df data.frame where variable should be added
+#' @param var variable name of the numerical staus-variable with values {-1,0,1}
+#' @param suffix wanted suffix in the added variable's name :  ferdigstilt_`suffix`
+#' @return data.frame with new variable
 #'
-#' @return
 #' @export
-#'
-#' @examples
+#' @example
+#' x <- data.frame(SkjemaStatusStart = c(-1,-1,0,0,1,-1,0,1,NA),
+#'                 SkjemastatusHovedskjema = rep(0,9),
+#'                 SkjemaStatusUtskrivelse = rep(-1,9),
+#'                 SkjemaStatusKomplikasjoner = c(rep(-1,4), rep(1,3), NA, NA))
+#' x %>% utlede_ferdigstilt(., var = SkjemaStatusStart, suffix = "startSkjema")
+#' x %>%
+#' utlede_ferdigstilt(., var = SkjemaStatusStart, suffix = "startSkjema") %>%
+#' utlede_ferdigstilt(., var = SkjemastatusHovedskjema, suffix = "hovedSkjema") %>%
+#' utlede_ferdigstilt(., var = SkjemaStatusKomplikasjoner, suffix = "komplikSkjema")
+
 utlede_ferdigstilt <- function(df,
                                var = SkjemaStatusStart,
                                suffix = "startSkjema"){
