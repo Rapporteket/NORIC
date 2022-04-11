492dff2f81c6f030e66981455b612f2a2d710468
Kristina Skåre <kristina.skare@helse-bergen.no>
2022-04-11 11:10:10 +0200

    Funksjon som bearbeider kodebok/variabler

---
+++ R/getKodebokData.R
@@ -0,0 +1,11 @@
+#' Kodebok med utledete variabler
+#'
+#' Legg til utledete variabler i kodebok for NORIC
+#' 
+#' @return data.frame med kodebok for NORIC med utledete variabler
+#' @export
+#'
+#' @examples
+getKodebokMedUtledeteVariabler <- function() {
+  noric::def_utledete_var
+}
