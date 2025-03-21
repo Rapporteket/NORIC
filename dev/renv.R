288c2adcd3c280ace3a57b8acc283601318cfabd
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2025-03-21 13:17:07 +0100

    div dev-greier

---
+++ dev/renv.R
@@ -0,0 +1,8 @@
+Sys.setlocale(locale = 'nb_NO.UTF-8')
+Sys.setenv(R_RAP_INSTANCE="DEV")
+Sys.setenv(R_RAP_CONFIG_PATH="C:/Users/ast046/repo/rapporteket/noric/dev/conf")
+Sys.setenv(DB_HOST="localhost")
+Sys.setenv(DB_USER="root")
+Sys.setenv(DB_PASS="root")
+Sys.setenv(USERID="")
+
