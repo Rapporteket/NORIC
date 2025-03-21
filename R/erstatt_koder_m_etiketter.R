910cdbb98d618e496fce870e40464d2732c8ef34
Thon Kevin Otto <Kevin.Otto.Thon@helse-nord.no>
2025-03-21 12:22:48 +0100

    Lagt til funksjon for å erstatte koder i tabellen angiopcinum med tekstetiketter. Dette for å gjenskape tabellen angiopci.

---
+++ R/erstatt_koder_m_etiketter.R
@@ -0,0 +1,21 @@
+#' Erstatt kode med etiketter i data.frame basert på kodebok
+#'
+#' @param df data.frame, er kun kvalitetssikret til å fungere med angiopcinum
+#' @return data.frame med etiketter i stedet for koder
+#'
+#' @export
+#' 
+erstatt_koder_m_etiketter <- function(df) {
+  df_label <- df |> 
+    dplyr::mutate(
+      dplyr::across(
+        intersect(names(df), noric::map_num_tekst$variabel_id), 
+        ~ factor(.x, 
+                 levels = dplyr::filter(noric::map_num_tekst, 
+                                        variabel_id %in% 
+                                          dplyr::cur_column())$verdi,
+                 labels = dplyr::filter(noric::map_num_tekst, 
+                                        variabel_id %in% 
+                                          dplyr::cur_column())$verditekst))
+    )
+}
