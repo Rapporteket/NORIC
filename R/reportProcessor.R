8f57b9f142c4dac83f5827985292cb15794500e7
Kristina Skåre <kristina.skare@helse-bergen.no>
2025-11-10 13:54:03 +0100

    fix nasjonal/lokal for indiktor-rapport

--- R/reportProcessor.R
+++ R/reportProcessor.R
@@ -60,17 +60,7 @@ reportProcessor <- function(report,
                           "NORIC_filvask_avdod", 
                           "NORIC_tavi_report"))
 
-  stopifnot(registryName %in% c("noric_nasjonal",
-                          "noric_ahus",
-                          "noric_bergen",
-                          "noric_bodoe", 
-                          "noric_feiring", 
-                          "noric_ous_rh", 
-                          "noric_ous_ull", 
-                          "noric_soerlandet",
-                          "noric_stavanger", 
-                          "noric_stolav", 
-                          "noric_unn"))
+  stopifnot(registryName %in% "noric_bergen")
   
   
   stopifnot(outputType %in% c("html", "pdf"))
