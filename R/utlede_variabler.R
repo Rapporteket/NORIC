2742b3470822fbc31219944ba62a2acaaa62806d
Kristina Skåre <kristina.skare@helse-bergen.no>
2026-01-29 13:16:55 +0100

    nye fn for prosedyrerelatert død

--- R/utlede_variabler.R
+++ R/utlede_variabler.R
@@ -185,9 +185,6 @@ utlede_dod_noric <- function(df_ap){
   
 }
 
-
-
-
 #' Minst en gang død under NORIC-oppholdet
 #'
 #' Funksjonen grupperer primær- og sekundærforløp sammen, ved hjelp 
@@ -235,12 +232,105 @@ avdod_opphold <- function(df_ap) {
                   "dod_noric") %in% names(df_ap)))
   
   df_ap %>%
-      dplyr::group_by(.data$AvdRESH, .data$OppholdsID) %>%
+    dplyr::group_by(.data$AvdRESH, .data$OppholdsID) %>%
     dplyr::mutate(
       dod_opphold = ifelse(
         all(.data$dod_noric == "Nei"),
         "Nei",
         "Ja")) %>%
     dplyr::ungroup()
+}
+
+#' Minst en registrering av prosedyrerelatert død under forløpet
+#'
+#'Funksjonen \code{utlede_prosedyrerelatertdod_noric} genererer en 
+#'ny variabel i AP-datasettet:
+#' \code{dod_prosedyrerelatert_noric}. Dersom minst en av variablene 
+#'\code{LabKompProsedyrerelatertDod} eller \code{AvdKompProsedyrerelatertDod} 
+#'har verdien \emph{Ja},  så regnes pasienten som prosedyrerelatert død under 
+#'forløpet. Gjelder alle typer forløp, primær og sekundær. 
+#'Brukes sammen med funksjonen \code{avdod_prosedyrerelatert_opphold} for å gruppere 
+#'NORIC-opphold sammen. 
+#'
+#'@param df_ap data.frame med AngioPCI-data tabellen. Må inneholde variablene
+#'\code{LabKompProsedyrerelatertDod} og \code{AvdKompProsedyrerelatertDod}.
+#'@return Funksjonen returnerer \code{ap_df}, med en ny kolonne
+#'ved navn \code{dod_prosedyrerelatert_noric}.
+#' @examples
+#' x <- data.frame(LabKompProsedyrerelatertDod = c("Ja", "Nei", "Ukjent", NA, 
+#'                                                 "Ja", "Nei", "Ukjent", NA), 
+#'                 AvdKompProsedyrerelatertDod = c("Ja","Ja", 
+#'                                                 "Nei", "Nei", 
+#'                                                 "Ukjent", "Ukjent", 
+#'                                                 NA, NA))
+#' noric::utlede_dod_prosedyrerelatert_noric(x)                                               
+#' @export
+utlede_dod_prosedyrerelatert_noric <- function(df_ap){
+  stopifnot(all(c("LabKompProsedyrerelatertDod",
+                  "AvdKompProsedyrerelatertDod") %in% names(df_ap)))
+  
+  df_ap %>% 
+    dplyr::mutate(
+      dod_prosedyrerelatert_noric = dplyr::if_else(
+        condition = (LabKompProsedyrerelatertDod %in% "Ja" |
+                       AvdKompProsedyrerelatertDod %in% "Ja"),
+        true = "Ja",
+        false = "Nei",
+        missing = "Nei"))
   
 }
+
+#' Minst en gang prosedyrerelatert død under NORIC-oppholdet
+#'
+#' Funksjonen grupperer primær- og sekundærforløp sammen, ved hjelp 
+#' av \code{OppholdsID}. En ny variabel blir generert i AP-tabellen: 
+#' \code{dod_prosedyrerelatert_opphold}. Denne variabelen får verdien \emph{Ja} dersom  
+#' \code{dod_prosedyrerelatert_noric} har verdien \emph{Ja} for minst et av forløpene med felles
+#' \code{OppholdsID} og får verdien \emph{Nei} dersom ingen av forløpene 
+#' med felles \code{OppholdsID} har registrering av avdød. Funksjonen brukes
+#' sammen med \code{noric::utlede_OppholdsID} og 
+#' \code{noric::utlede_dod_prosedyrerelatert_noric}.
+#' 
+#' @param df_ap data.frame med AngioPCI-data tabellen. Må inneholde variablene 
+#'\code{AvdRESH}, \code{OppholdsID} og \code{dod_prosedyrerelatert_noric}. 
+#' @return Funksjonen returnerer \code{ap_df}, med en ny kolonne
+#' ved navn \code{dod_prosedyrerelatert_opphold}.
+#'
+#' @examples
+#' x <- data.frame(AvdRESH = rep(123456, 8), 
+#'                ForlopsID = 1:8, 
+#'                Regtype = c("Primær", "Sekundær", rep("Primær", 4), 
+#'                            "Sekundær", "Sekundær"), 
+#'                PrimaerForlopsID = c(1, 1, 2, 2, 3, 4, 4, 4), 
+#'                LabKompProsedyrerelatertDod = c("Nei", "Nei", "Ukjent", NA, 
+#'                                                "Ja", "Nei", "Ukjent", "Ja"), 
+#'                AvdKompProsedyrerelatertDod = c("Nei","Ja", 
+#'                                                "Nei", "Nei", 
+#'                                                "Ukjent", "Ukjent", 
+#'                                                 NA, NA))
+#' x %>%  
+#'   noric::utlede_OppholdsID(.) %>% 
+#'   noric::utlede_dod_prosedyrerelatert_noric(.) %>% 
+#'   noric::avdod_prosedyrerelatert_opphold(.)  %>% 
+#'   dplyr::arrange(OppholdsID)              
+#'
+#'
+#' @export
+avdod_prosedyrerelatert_opphold <- function(df_ap) {
+  
+  stopifnot(all(c("AvdRESH",
+                  "OppholdsID",
+                  "dod_prosedyrerelatert_noric") %in% names(df_ap)))
+  
+  df_ap %>%
+    dplyr::group_by(.data$AvdRESH, .data$OppholdsID) %>%
+    dplyr::mutate(
+      dod_prosedyrerelatert_opphold = ifelse(
+        all(.data$dod_prosedyrerelatert_noric == "Nei"),
+        "Nei",
+        "Ja")) %>%
+    dplyr::ungroup()
+}
+
+
+
