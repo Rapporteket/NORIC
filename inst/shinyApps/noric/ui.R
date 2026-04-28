d7a11d58cd75ff0e1259adbe88993aefd0b0ff8d
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2026-04-28 10:27:28 +0200

    Endre fra theme/title til rapTheme/regTitle (#285)
    Spesifiser minste versjon av rapbase

--- inst/shinyApps/noric/ui.R
+++ inst/shinyApps/noric/ui.R
@@ -10,9 +10,9 @@ regTitle <- "NORIC"
 
 ui <- shiny::tagList(
   shiny::navbarPage(
-    title = rapbase::title(regTitle),
+    title = rapbase::regTitle(regTitle),
     windowTitle = regTitle,
-    theme = rapbase::theme(),
+    theme = rapbase::rapTheme(),
     id = "tabs",
 
     shiny::tabPanel(
