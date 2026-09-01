6eadbf8b4220dbb8c2fd8c596a2e5a8152385f21
Mary Gerina Autenried <mary.gerina.autenried@helse-bergen.no>
2026-09-01 10:23:10 +0200

    Byttet ut kode

--- R/getData.R
+++ R/getData.R
@@ -957,8 +957,7 @@ getRawData <- function(registryName, tableName, ...){
   query <- noric::queryRawData(tableName)
   msg <- "Query rawdata"
   
-  if ("session" %in% names(list(...))) {
-    rapbase::repLogger(session = list(...)[["session"]], msg = msg)
-  }
+  rapbase::repLogger(session = NULL, msg = msg)
+  
   return(rapbase::loadRegData(registryName, query))
 }
