20de1bc3d8e4460ccccb197e169eaff3037764e8
Mary Gerina Autenried <mary.gerina.autenried@helse-bergen.no>
2026-09-08 12:17:59 +0200

    fiks av tildACB

--- R/getQuery.R
+++ R/getQuery.R
@@ -2035,7 +2035,7 @@ queryApLight <- function(){
      A.WEIGHT AS Vekt,
      A.SKREATININ AS SKreatinin,
      A.TIDPCI  AS TidlPCI,
-     A.TIDCABG  AS TidlABC,
+     A.TIDCABG  AS TidlACB,
      A.SMOKING_STATUS  AS RoykeStatus,
      A.HYPERTON  AS BehHypertoni,
      A.STATINS AS Statiner,
